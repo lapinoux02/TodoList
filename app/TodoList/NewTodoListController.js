349922d833898c9a78f17db4a0e1933eec784460
lapinoux02 <paulbnt@free.fr>
2015-12-31 19:40:28 +0100

    codate du cancel sur la creation du newTodoList

--- app/TodoList/NewTodoListController.js
+++ app/TodoList/NewTodoListController.js
@@ -4,4 +4,9 @@ angular.module('TODO')
         var todoListIndex = DataService.createNewTodoList($scope.newTodoListTitle);
         $state.go('TodoList', {todoListId: todoListIndex});
     };
+    
+    $scope.cancelNewTodo = function() {
+        $scope.newTodoListTitle = "";
+        $state.go('Home');
+    };
 });
